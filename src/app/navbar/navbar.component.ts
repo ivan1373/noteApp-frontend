8eb0efd06d33a4a3cb8f0aacbd46533636bf4ad5
Ivan Miloš <34128933+ivan1373@users.noreply.github.com>
2019-07-06 13:34:02 +0200

    ...

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -14,7 +14,7 @@ export class NavbarComponent implements OnInit {
   
 
   constructor(private router: Router,
-    public authenticationService: AuthService) {
+    private authenticationService: AuthService) {
       
      }
 
